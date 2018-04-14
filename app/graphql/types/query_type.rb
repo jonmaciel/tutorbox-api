f2c021b8ed675a7c678d833d8110ef611357cdb8
Jon Maciel <joaomaciel.n@gmail.com>
2018-04-14 17:30:43 -0300

    adds member wuery and improvs s3 things

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -80,6 +80,20 @@ Types::QueryType = GraphQL::ObjectType.define do
     end
   end
 
+  field :selectMembers, types[Types::UserType] do
+    argument :organizationId, !types.ID, 'Video ID'
+    description 'Get organizations'
+    resolve ->(_, input, context) do
+      organization = Organization.find(input[:organizationId])
+
+      context[:current_user].authorize!(:show, organization)
+
+      return users(system: current_user.organization.systems) if current_user.end_user?
+      return users.or(User.where(organization: nil)) if current_user.admin? || current_user.script_writer?
+      []
+    end
+  end
+
   field :assignedVideos, types[Types::VideoType] do
     description 'Get organizations'
     argument :userId, types.ID, 'User ID'
@@ -94,19 +108,20 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :s3SignedUrl, Types::S3Type do
     argument :fileType, types.String, 'User ID'
+    argument :videoId, !types.ID, 'User ID'
     resolve ->(_, input, context) do
       return unless input[:fileType]
 
       require 'aws-sdk-s3'
 
-      new_name = "#{SecureRandom.hex}.#{input[:fileType].split('/').last}"
+      new_name = "video_attachments/#{input[:videoId]}/#{SecureRandom.hex}.#{input[:fileType].split('/').last}"
 
       Aws.config.update({
-        region: 'us-east-1',
+        region: 'us-east-2',
         credentials: Aws::Credentials.new('AKIAIDV3FJF3WMG5GHHA', 'q2tiO8ZKhbr/FvIkZYj5Ozl+9qhPl9DqLc3rAu6Z')
       })
 
-      s3 = Aws::S3::Resource.new.bucket('tutorboxfiles')
+      s3 = Aws::S3::Resource.new.bucket('tutorbox-files')
 
       object = s3.object(new_name)
 
--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -4,4 +4,5 @@ describe Types::QueryType do
   it { is_expected.to have_field(:organization).of_type(Types::OrganizationType) }
   it { is_expected.to have_field(:organizations).of_type([Types::OrganizationType]) }
   it { is_expected.to have_field(:assignedVideos).of_type([Types::VideoType]) }
+  it { is_expected.to have_field(:selectMembers).of_type([Types::UserType]) }
 end
